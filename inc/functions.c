8ab56543e82458cc1ea5f4be5d26642e42f94c2d
cisco <mathieu.brisset@laposte.net>
2016-12-15 09:28:54 +0100

    Update functions.c

--- inc/functions.c
+++ inc/functions.c
@@ -1,72 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
-#include <unistd.h>
 
-void display(Screensaver scrnsave) {
+/* ==================== RANDOM ==================== */
+int randomnb(int MIN, int MAX) {
+  int nombreAleatoire = 0;
 
-	FILE *fichier = NULL;
-	int i = 0;
-	char caractere;
-	char c;
-	char chaine[10];
+  srand(time(NULL));
+  nombreAleatoire = (rand() % (MAX - MIN + 1)) + MIN; // Génère un nombre aléatoire entre MIN et MAX
 
-	int test = 0;
-	int dimension[2];
-
-	int width = 0;
-	int height = 0;
-	int size = 0;
-	char *tableau;
-
-	if(scrnsave.stat != NULL)
-		fichier = fopen(scrnsave.stat.file, "r"); // OUVRE LE FICHIER EN LECTURE
-	else if(scrnsave.dyn != NULL)
-		fichier = fopen(scrnsave.dyn.file, "r"); // OUVRE LE FICHIER EN LECTURE
-	else if(scrnsave.dyn != NULL)
-		fichier = fopen(scrnsave.inter.file, "r"); // OUVRE LE FICHIER EN LECTURE
-	else {
-		printf("Le fichier ne peut pas être ouvert\n");
-		return;
-	}
-
-	/* =====================================================
-	==================== TAILLE FICHIER ====================
-	===================================================== */
-	  if (fichier != NULL) { // SI L'IMAGE EST BIEN RECUE
-	    fseek (fichier, 8, SEEK_SET); // SKIP LES PREMIERS ELEMENTS POUR ALLER A L'IMAGE 0/1
-	    fscanf(fichier, "%d %d", &dimension[0], &dimension[1]);
-	    width = dimension[0];
-	    height = dimension[1];
-	    size = 2*width*height;
-	    tableau = malloc(size * sizeof(char));
-
-
-	/* ========================================================
-	==================== AFFICHAGE FICHIER ====================
-	======================================================== */
-	    do {
-	      caractere = fgetc(fichier); // AFFECTE LA LIGNE DANS CHAINE
-	      if (caractere == '0') {caractere = ' '; tableau[i] = caractere; i++;} // SI IL Y A UN 0 DANS LE FICHIER PBM -> LE REMPLACER PAR UN ESPACE
-	      else if (caractere == ' ') {caractere = ' '; tableau[i] = caractere; i++;} // MET LESPACE DANS LE TABLEAU
-	      else if (caractere == '\n') {caractere = '\n'; tableau[i] = caractere; i++;} // MET LE RETOUR A LA LIGNE DANS LE TABLEAU
-	      else if (caractere == '1') {caractere = 'X'; tableau[i] = caractere; i++;} // SI IL Y A UN 1 DANS LE FICHIER PBM -> LE REMPLACER PAR UN X
-	    } while (caractere != EOF); // LIT JUSQU'A LA FIN DU FICHIER
-
-	    for(int j=0; j<size; j++) {
-			printf("%c", tableau[j]);
-
-	    }
-
-		free(tableau);
-	}
-
-
-	/* =====================================================
-	==================== ERREUR FICHIER ====================
-	===================================================== */
-	else {printf("Le fichier ne peut pas être ouvert\n");} // SI L'IMAGE N'EST PAS RECUE
-
-
+  return nombreAleatoire; // Retourne le nombre aléatoire
 }
+/* ==================== RANDOM ==================== */
+
+
+
+
+/* ==================== DIMENSIONS PBM ==================== */
+int widthPBM(FILE* fichier) {
+  int width = 0; // Largeur
+  int height = 0; // Hauteur
+
+  fseek (fichier, 8, SEEK_SET); // Skip les premiers éléments pour aller aux dimensions de l'image
+  fscanf(fichier, "%d %d", &width, &height); // Récupère les dimensions de l'image
+
+  return width; // Renvoie la largeur de l'image
+}
+
+int heightPBM(FILE* fichier) {
+  int width = 0; // Largeur
+  int height = 0; // Hauteur
+
+  fseek (fichier, 8, SEEK_SET); // Skip les premiers éléments pour aller aux dimensions de l'image
+  fscanf(fichier, "%d %d", &width, &height); // Récupère les dimensions de l'image
+
+  return height; // Renvoie la hauteur de l'image
+}
+/* ==================== DIMENSIONS PBM ==================== */
+
+
+
+
+/* ==================== DISPLAY PBM ==================== */
+#define LARGEUR_CONSOLE 80
+#define HAUTEUR_CONSOLE 22
+
+void centerX(int width, const char* str) {
+  for (int a = 0; a < ((LARGEUR_CONSOLE - (width*2)) / 2); a++) {
+    putchar(' '); // Ajoute un espace tant que l'image n'est pas centrée horizontalement
+  }
+  printf("%s", str); // Affiche l'image
+}
+
+void centerY(int height) {
+  for (int b = 0; b < ((HAUTEUR_CONSOLE - height) / 2); b++) {
+    putchar('\n'); // Ajoute un retour à la ligne tant que l'image n'est pas centrée verticalement
+  }
+}
+
+void displayPBM(FILE* fichier, int size, int width, int height, char *tableau) {
+  tableau = malloc(size * sizeof(char)); // Le tableau prend la taille de l'image
+
+  centerY(height);
+  for (int i = 0; i < (height + 1); i++) { // Parcourt toutes les lignes
+    fgets(tableau, size, fichier); // Rentre les caractères dans le tableau
+
+    for (int j = 0; j < size; j++) { // Parcourt toutes les colonnes
+      if (tableau[j] == '0') {tableau[j] = ' ';} // Si il y a un 0 dans le fichier PBM -> Le remplace par un ESPACE
+      else if (tableau[j] == '1') {tableau[j] = 'X';} // Si il y a un 1 dans le fichier PBM -> Le remplace par un X
+    }
+
+    centerX(width, tableau); // Affiche l'image centrée
+  }
+
+  free(tableau);
+}
+/* ==================== DISPLAY PBM ==================== */
