ddf3dd3446f393020022e5c3c6312d9a6aa69a3c
cisco <mathieu.brisset@laposte.net>
2016-12-15 09:27:08 +0100

    Update functions.h

--- inc/functions.h
+++ inc/functions.h
@@ -26,6 +26,9 @@ struct Interactive {
 	char *file[50];
 };
 
-void display(Screensaver scrnsave);
-
-void movePic(Dynamic dyn);
+int randomnb(int MIN, int MAX);
+int widthPBM(FILE* fichier);
+int heightPBM(FILE* fichier);
+void centerX(int width, const char* str);
+void centerY(int height);
+void displayPBM(FILE* fichier, int size, int width, int height, char *tableau);
