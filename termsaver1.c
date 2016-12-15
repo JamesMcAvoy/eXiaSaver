f6e7d9da80717bd04bf39d0f3156912b1c5e1f83
clemckr <clemckr@gmail.com>
2016-12-15 17:05:21 +0100

    Update termsaver1.c

--- termsaver1.c
+++ termsaver1.c
@@ -1,80 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <unistd.h>
+#include <wait.h>
 #include <string.h>
 
-
-/* ==================== RANDOM ==================== */
-int randomnb(int MIN, int MAX) {
-  int nombreAleatoire = 0;
-
-  srand(time(NULL));
-  nombreAleatoire = (rand() % (MAX - MIN + 1)) + MIN; // Génère un nombre aléatoire entre MIN et MAX
-
-  return nombreAleatoire; // Retourne le nombre aléatoire
-}
-/* ==================== RANDOM ==================== */
+#include "fonctions.h"
 
 
+int main(int argc, char *argv[]) {
+/* ==================== CLEAR ==================== */
+  system("clear"); // EFFACE LE TERMINAL
 
 
-/* ==================== DIMENSIONS PBM ==================== */
-int widthPBM(FILE* fichier) {
+/* ==================== VARIABLES ==================== */
+  /* FICHIER / TAILLE */
+  FILE* fichier = NULL;
+  fichier = NULL;
   int width = 0; // Largeur
   int height = 0; // Hauteur
+  int size = 0; // Taille de l'image
+  char *tableau; // Tableau permattant de stocker l'image
 
-  fseek (fichier, 8, SEEK_SET); // Skip les premiers éléments pour aller aux dimensions de l'image
-  fscanf(fichier, "%d %d", &width, &height); // Récupère les dimensions de l'image
-
-  return width; // Renvoie la largeur de l'image
-}
-
-int heightPBM(FILE* fichier) {
-  int width = 0; // Largeur
-  int height = 0; // Hauteur
-
-  fseek (fichier, 8, SEEK_SET); // Skip les premiers éléments pour aller aux dimensions de l'image
-  fscanf(fichier, "%d %d", &width, &height); // Récupère les dimensions de l'image
-
-  return height; // Renvoie la hauteur de l'image
-}
-/* ==================== DIMENSIONS PBM ==================== */
+  /* GENERATION DE L'IMAGE */
+  int MIN = 1; // Numéro de l'image le plus bas
+  int MAX = 5; // Numéro de l'image le plus haut
+  int nombreAleatoire;
+  //const char *EXIASAVER_HOME;
+  char EXIASAVER1_PBM[50];
+  //getenv("EXIASAVER1_PBM");
+  //const char *EXIASAVER2_PBM;
+  //const char *EXIASAVER3_PBM;
+  strcpy(EXIASAVER1_PBM, "/PBM");
+  char chaine[50];
 
 
-
-
-/* ==================== DISPLAY PBM ==================== */
-#define LARGEUR_CONSOLE 80
-#define HAUTEUR_CONSOLE 22
-
-void centerX(int width, const char* str) {
-  for (int a = 0; a < ((LARGEUR_CONSOLE - (width*2)) / 2); a++) {
-    putchar(' '); // Ajoute un espace tant que l'image n'est pas centrée horizontalement
+  nombreAleatoire = randomnb(MIN, MAX); // Génère un nombre aléatoire entre MIN et MAX
+  switch(nombreAleatoire) {
+    case 1: strcpy(chaine, EXIASAVER1_PBM); strcat(chaine, "/image_1.pbm"); break;
+    case 2: strcpy(chaine, EXIASAVER1_PBM); strcat(chaine, "/image_2.pbm"); break;
+    case 3: strcpy(chaine, EXIASAVER1_PBM); strcat(chaine, "/image_3.pbm"); break;
+    case 4: strcpy(chaine, EXIASAVER1_PBM); strcat(chaine, "/image_4.pbm"); break;
+    case 5: strcpy(chaine, EXIASAVER1_PBM); strcat(chaine, "/image_5.pbm"); break;
   }
-  printf("%s", str); // Affiche l'image
-}
+  //sprintf(chaine, "../PBM1/image_%d.pbm", nombreAleatoire); // Stocke le chemin dans la variable <chaine>
+  fichier = fopen(chaine, "r"); // Ouvre le fichier en lecture
+/* ==================== VARIABLES ==================== */
 
-void centerY(int height) {
-  for (int b = 0; b < ((HAUTEUR_CONSOLE - height) / 2); b++) {
-    putchar('\n'); // Ajoute un retour à la ligne tant que l'image n'est pas centrée verticalement
-  }
-}
 
-void displayPBM(FILE* fichier, int size, int width, int height, char *tableau) {
-  tableau = malloc(size * sizeof(char)); // Le tableau prend la taille de l'image
+  if (fichier != NULL) { // Si le fichier est bien reçu
+/* ==================== TAILLE FICHIER ==================== */
+    width = widthPBM(fichier); // Récupère la largeur l'image (sans les espaces)
+    height = heightPBM(fichier); // Récupère la hauteur de l'image
+    size = 2*width*height; // Récupère la taille de l'image (avec les espaces)
 
-  centerY(height);
-  for (int i = 0; i < (height + 1); i++) { // Parcourt toutes les lignes
-    fgets(tableau, size, fichier); // Rentre les caractères dans le tableau
 
-    for (int j = 0; j < size; j++) { // Parcourt toutes les colonnes
-      if (tableau[j] == '0') {tableau[j] = ' ';} // Si il y a un 0 dans le fichier PBM -> Le remplace par un ESPACE
-      else if (tableau[j] == '1') {tableau[j] = 'X';} // Si il y a un 1 dans le fichier PBM -> Le remplace par un X
+/* ==================== AFFICHAGE FICHIER ==================== */
+    pid_t fils; // Initialisation du processus fils
+    int father; // Initialisation du processus père
+    fils = fork(); // Créé le processus fils
+
+    if(fils == 0) { // Exécute le code dans le processus fils
+      displayPBM(fichier, size, width, height, tableau); // Affiche l'image
+      system("stty cbreak -echo");
+      getchar(); // Attend que l'utilisateur appuie sur une touche
+      system("stty cooked echo"); // Permet de fermer le programme sans appuyer sur ENTRER
     }
+    else {father = wait(NULL); system("clear"); return EXIT_SUCCESS;} // Permet de retourner dans le processus père à la fin du programme
 
-    centerX(width, tableau); // Affiche l'image centrée
   }
 
-  free(tableau);
+  else {printf("Le fichier n'a pas pu être reçu...\n"); return EXIT_FAILURE;}
 }
-/* ==================== DISPLAY PBM ==================== */
