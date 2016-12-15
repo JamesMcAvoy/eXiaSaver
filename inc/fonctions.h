be1497e955e7476b44c5bf73d8c1b39022b084c6
cisco <mathieu.brisset@laposte.net>
2016-12-15 09:31:13 +0100

    Delete fonctions.h

--- inc/fonctions.h
+++
@@ -1,6 +0,0 @@
-int randomnb(int MIN, int MAX);
-int widthPBM(FILE* fichier);
-int heightPBM(FILE* fichier);
-void centerX(int width, const char* str);
-void centerY(int height);
-void displayPBM(FILE* fichier, int size, int width, int height, char *tableau);
