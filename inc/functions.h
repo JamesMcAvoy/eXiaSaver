d39b5f3454f328902b6b2d40c30e94e01e54e8b4
clemckr <clemckr@gmail.com>
2016-12-15 17:00:28 +0100

    Update functions.h

--- inc/functions.h
+++ inc/functions.h
@@ -32,3 +32,4 @@ int heightPBM(FILE* fichier);
 void centerX(int width, const char* str);
 void centerY(int height);
 void displayPBM(FILE* fichier, int size, int width, int height, char *tableau);
+void initialisationHeure();
