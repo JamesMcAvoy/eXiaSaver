0300ee6841f52e3896f87c5b976006403526e1d9
clemckr <clemckr@gmail.com>
2016-12-16 13:52:10 +0100

    histo fonctionnel

---
+++ inc/fonctions.h
@@ -0,0 +1,8 @@
+int randomnb(int MIN, int MAX);
+int widthPBM(FILE* fichier);
+int heightPBM(FILE* fichier);
+void centerX(int width, const char* str);
+void centerY(int height);
+void displayPBM(FILE* fichier, int size, int width, int height, char *tableau);
+void initialisationHeure();
+typedef struct Historical Historical;
